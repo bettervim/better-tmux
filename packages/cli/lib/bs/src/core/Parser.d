048758df868bb99232476b0ba7d9121884683e63
Marcos Oliveira <marcos.oliveira@vtex.com>
2024-07-04 20:07:32 -0300

    *

--- packages/cli/lib/bs/src/core/Parser.d
+++
@@ -1,1 +0,0 @@
-src/core/Parser.cmj : src/core/Reconcilier.cmj src/core/Reconcilier.cmi src/core/Styles.cmj src/core/Styles.cmi src/core/TmuxJsx.cmj src/core/TmuxJsx.cmi
--- packages/cli/lib/bs/src/core/Reconcilier.d
+++
@@ -1,1 +0,0 @@
-src/core/Reconcilier.cmj : src/bindings/ReactReconcilier.cmj src/bindings/ReactReconcilier.cmi src/core/TmuxJsx.cmj src/core/TmuxJsx.cmi
