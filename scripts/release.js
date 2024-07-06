44211a9ce07964ffd0e24ee39c534b661508310e
Marcos Oliveira <marcos.oliveira@vtex.com>
2024-07-06 16:40:04 -0300

    chore: updated release script

--- scripts/release.js
+++ scripts/release.js
@@ -1,3 +1,4 @@
+
 import { execSync } from "child_process";
 import { readFileSync, writeFileSync } from "fs";
 import { join } from "path";
@@ -10,39 +11,32 @@ const bumpPatchVersion = (version) => {
 const readJson = (filePath) => JSON.parse(readFileSync(filePath, "utf-8"));
 const writeJson = (filePath, data) => writeFileSync(filePath, JSON.stringify(data, null, 2));
 
-const executeGitCommands = (filePath, projectName, tagName) => {
+const executeGitCommands = (filePath, tagName) => {
   execSync(`git add ${filePath}`);
-  execSync(`git commit -m "chore(${projectName}): bump version to ${tagName.split('@')[1]}"`);
+  execSync(`git commit -m "chore: bump version to ${tagName}"`);
   execSync(`git tag ${tagName}`);
   execSync(`git push --follow-tags`);
 };
 
-const updateVersionAndCommit = (projectPath, projectName, tagPrefix) => {
+const updateVersionAndCommit = (projectPath) => {
   const packageJsonPath = join(projectPath, "package.json");
   const packageJson = readJson(packageJsonPath);
   packageJson.version = bumpPatchVersion(packageJson.version);
   writeJson(packageJsonPath, packageJson);
-  const tagName = `${tagPrefix}@${packageJson.version}`;
-  executeGitCommands(packageJsonPath, projectName, tagName);
-  console.log(`Bumped version of ${projectName} to ${packageJson.version}, created tag ${tagName}, and pushed to remote`);
+  const tagName = `v${packageJson.version}`;
+  executeGitCommands(packageJsonPath, tagName);
+  console.log(`Bumped version to ${packageJson.version}, created tag ${tagName}, and pushed to remote`);
 };
 
-const main = (projectName) => {
-  const projectPaths = {
-    lib: "packages/lib",
-    cli: "packages/cli",
-  };
+const main = () => {
+  const projects = [
+    { path: "packages/lib" },
+    { path: "packages/cli" }
+  ];
 
-  const tagPrefixes = {
-    lib: "better-tmux",
-    cli: "cli",
-  };
-
-  const projectPath = projectPaths[projectName];
-  const tagPrefix = tagPrefixes[projectName];
-  projectPath && tagPrefix ? updateVersionAndCommit(projectPath, projectName, tagPrefix) : console.error(`Invalid project name: ${projectName}`);
+  projects.forEach(project => {
+    updateVersionAndCommit(project.path);
+  });
 };
 
-const [projectName] = process.argv.slice(2);
-projectName ? main(projectName) : console.error("Usage: node script.js <project-name>");
-
+main();
