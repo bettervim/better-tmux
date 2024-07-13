3ac2d657ec4b522dc544e7f5a22dc2c17002d5e7
Marcos Oliveira <marcos.oliveira@vtex.com>
2024-07-13 01:17:32 -0300

    fix(presets): fixed window bg and fg colors

--- packages/lib/src/presets/index.tsx
+++ packages/lib/src/presets/index.tsx
@@ -1,7 +1,9 @@
+import { useTheme } from '../hooks/use-theme.js'
 import { BetterTmuxConfig, WindowConfig } from '../types.js'
 
 const Window = ({ type, number, name }: WindowConfig) => {
-  let style = type === "active" ? { bg: "#fafafa", fg: "#000000" } : {}
+  const theme = useTheme()
+  let style = type === "active" ? { bg: theme.primary, fg: theme.foreground } : {}
 
   return (
     <box padding={1} {...style}>
